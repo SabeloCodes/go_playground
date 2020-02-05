51760d35d09de644b384847054d454d1f64afb3a
Ubuntu <ubuntu@ip-172-31-85-97.ec2.internal>
2020-02-05 21:04:41 +0000

    Changed main.js and payground.html to fix carousel

--- static/js/main.js
+++ static/js/main.js
@@ -1,28 +1,19 @@
-/*==========================CAROUSEL JAVASCRIPT=================================*/
+/*==========================INITIALIZE SELECT/CAROUSEL =================================*/
 
-//     $('.carousel').carousel({
-//     interval: 2000
-//     })
-    
-//       $(document).ready(function(){
-//     $('.carousel').carousel();
-//      interval: 2000
-//   });
-    
-    
-/*==========================INITIALIZE SELECT ELEMENT=================================*/
+// CAROUSEL
+$('.carousel').carousel({
+  interval: 2000
+});
 
-   document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('select');
-    var instances = M.FormSelect.init(elems, options);
-  });
-  
-   $(document).ready(function(){
-    $('.carousel').carousel();
-     interval: 2000   
+
     $('select').formSelect();
     $('input#input_text, textarea#textarea2').characterCounter();
   });
+
+
+
+     
+
   
  
 	
