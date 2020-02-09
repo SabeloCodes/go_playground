a3e9a695dc212b29a85e706c7c29504ced7599b8
Ubuntu <ubuntu@ip-172-31-85-97.ec2.internal>
2020-02-09 09:14:25 +0000

    Added sideNav and using Materialize carousel

--- static/js/main.js
+++ static/js/main.js
@@ -1,19 +1,20 @@
 /*==========================INITIALIZE SELECT/CAROUSEL =================================*/
 
-$(document).ready(function(){
-      $('.carousel').carousel();
-      interval: 2000
-      $('.carousel.carousel-slider').carousel({fullWidth: true});
-    });
 
+$(document).ready(function(){
+    $('.carousel .carousel-slider .slider').carousel({
+        fullwidth: true,
+        interval: 2000,
+        autoplay:true,
+        indicators: true,
+        showarrows:true
+    });
     $('select').formSelect();
     $('input#input_text, textarea#textarea2').characterCounter();
+    $(".button-collapse").sideNav();
+});
 
 
 
 
-     
 
-  
- 
-	
