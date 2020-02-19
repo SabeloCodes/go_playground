14a77c8009433dc660fabbcd7356af5385430a70
Ubuntu <ubuntu@ip-172-31-85-97.ec2.internal>
2020-02-19 17:04:23 +0000

    Added Bootstrap 4 form and GoogleMaps API on showPlayground.html

--- static/js/main.js
+++ static/js/main.js
@@ -1,20 +1,20 @@
-/*==========================INITIALIZE SELECT/CAROUSEL =================================*/
+/*==========================INITIALIZE CAROUSEL =================================*/
 
 
 $(document).ready(function(){
     $('.carousel .carousel-slider .slider').carousel({
-        fullwidth: true,
-        interval: 2000,
-        autoplay:true,
-        indicators: true,
-        showarrows:true
+        interval: 4000,
     });
-    $('select').formSelect();
-    $('input#input_text, textarea#textarea2').characterCounter();
-    $(".button-collapse").sideNav();
-});
+  
 
-
-
-
-
+/*========================== MAP ==================================*/
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  var uluru = {lat: -25.344, lng: 131.036};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 4, center: uluru});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: uluru, map: map});
+}
