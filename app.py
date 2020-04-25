4f9047116eafe8cfff207a3e733b7399c1e542cb
Ubuntu <ubuntu@ip-172-31-85-97.ec2.internal>
2020-04-25 05:51:42 +0000

    Created editplayground.html and binded data to the edit playground form

--- app.py
+++ app.py
@@ -40,6 +40,15 @@ def insert_playground():
 def show_playground():
     return render_template("showplayground.html", 
                             playgrounds = mongo.db.playgrounds.find())
+                            
+#selects a playground and retreives from the database using its id and displays it in a form for editing
+@app.route('/edit_playground/<playground_id>')
+def edit_playground(playground_id):
+    #get the playground that matches the playground id '_id' is the key 
+    the_playground =  mongo.db.playgrounds.find_one({"_id": ObjectId(playground_id)})
+    all_boroughs =  mongo.db.boroughs.find()
+    return render_template('editplayground.html', playground=the_playground,
+                           boroughs=all_boroughs)
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
